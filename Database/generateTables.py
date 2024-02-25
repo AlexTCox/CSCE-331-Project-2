531abe2d67ffa9f038fe63f38ffac5264f6ea214
Alexander Cox <alexander_cox@tamu.edu>
2024-02-24 21:44:43 -0600

    Some changes to table attributes to add specific time and remove some unneeded attributes

--- Database/generateTables.py
+++ Database/generateTables.py
@@ -1,8 +1,4 @@
 import psycopg2
-import random
-import datetime
-from datetime import date, timedelta
-
 
 # Connecting to database:
 conn = psycopg2.connect(
@@ -15,62 +11,52 @@ conn = psycopg2.connect(
 
 # How we will execute SQL commands:
 cur = conn.cursor()
+conn.autocommit = True
 
 # Removing all the tables:
 cur.execute (
-    """DROP TABLE drink_order"""
+    """DROP TABLE drink_order;"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 cur.execute (
-    """DROP TABLE add_on_order"""
+    """DROP TABLE add_on_order;"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 cur.execute (
-    """DROP TABLE menu_item_ingredient"""
+    """DROP TABLE menu_item_ingredient;"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 cur.execute (
-    """DROP TABLE menu_order"""
+    """DROP TABLE menu_order;"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 cur.execute (
-    """DROP TABLE customer_order"""
+    """DROP TABLE customer_order;"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 cur.execute (
-    """DROP TABLE ingredients"""
+    """DROP TABLE ingredients;"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 cur.execute (
-    """DROP TABLE operator"""
+    """DROP TABLE operator;"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 cur.execute (
-    """DROP TABLE menu_item"""
+    """DROP TABLE menu_item;"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 cur.execute (
-    """DROP TABLE drinks"""
+    """DROP TABLE drinks;"""
 )
-# Commit changes to the database
-conn.commit()
-
 
 
 # Menu_Item table:
@@ -80,8 +66,7 @@ cur.execute (
     name text,
     price float);"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 # Operator table:
 cur.execute (
@@ -91,8 +76,7 @@ cur.execute (
     pin_hash int,
     admin bool);"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 # Ingredients table:
 cur.execute (
@@ -103,32 +87,27 @@ cur.execute (
     add_on_price float,
     min_stock int);"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 # drinks table
 cur.execute (
     """CREATE TABLE drinks (
     id serial primary key,
-    name text,
-    price float,
-    size text);"""
+    size text,
+    price float);"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 # Customer Order table:
 cur.execute (
     """CREATE TABLE customer_order (
     id serial primary key,
     cost float,
-    addOn bool,
-    date_of_sale date,
+    date_of_sale timestamp,
     operator_id serial,
     FOREIGN KEY (operator_id) REFERENCES operator(id));"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 # menu item and customer_order table
 cur.execute (
@@ -139,8 +118,7 @@ cur.execute (
     FOREIGN KEY (menu_item_id) REFERENCES menu_item(id),
     FOREIGN KEY (customer_order_id) REFERENCES customer_order(id));"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 # drink_order table
 cur.execute (
@@ -151,8 +129,7 @@ cur.execute (
     FOREIGN KEY (drink_id) REFERENCES drinks(id),
     FOREIGN KEY (customer_order_id) REFERENCES customer_order(id));"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 # menu item and ingredient table:
 cur.execute (
@@ -163,8 +140,7 @@ cur.execute (
     FOREIGN KEY (menu_id) REFERENCES menu_item(id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(id));"""
 )
-# Commit changes to the database
-conn.commit()
+
 
 # add-on Order
 cur.execute (
@@ -175,9 +151,7 @@ cur.execute (
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(id),
     FOREIGN KEY (customer_order_id) REFERENCES customer_order(id));"""
 )
-# Commit changes to the database
-conn.commit()
 
-# Close cursor and connection
+
 cur.close()
 conn.close()
