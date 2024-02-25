245d02d6821a46eaa9f25e20f1686730aa9b8a91
Alexander Cox <alexander_cox@tamu.edu>
2024-02-24 21:43:49 -0600

    Changed how the data is generated to not use csv and is overall much quicker

--- Database/generateData.py
+++ Database/generateData.py
@@ -1,62 +1,33 @@
-import csv
 import random
 import datetime
+import psycopg2
+
 from datetime import date, timedelta
 
+# Connecting to database:
+conn = psycopg2.connect(
+    database = "csce331_550_01_db",
+    user = "csce331_550_01_user",
+    host = "54.147.249.118",
+    password = "cSCUE8w9",
+    port = 5432
+)
 
-# Random values for menu, drink, add-on amounts:
-menuRand = random.randint(0,100)
-drinkRand = random.randint(0,100)
-add_onRand = random.randint(0,100)
-operatorRand = random.randint(0,1)
+cur = conn.cursor()
+conn.autocommit = True
 
-
-# Finding the amount of menu items:
-if menuRand <= 10:
-    menuAmount = 1
-elif menuRand > 10 and menuRand <= 25:
-    menuAmount = 3
-elif menuRand > 25 and menuRand <= 75:
-    menuAmount = 2
-elif menuRand > 75 and menuRand <= 89:
-    menuAmount = 3
-else:
-    menuAmount = 5
-
-# Finding the amount of drinks:
-if drinkRand <= 10:
-    drinkAmount = 0
-elif drinkRand > 10 and drinkRand <= 25:
-    drinkAmount = 1
-elif drinkRand > 25 and drinkRand <= 75:
-    drinkAmount = 1
-elif drinkRand > 75 and drinkRand <= 89:
-    drinkAmount = 2
-else:
-    drinkAmount = 0
-
-# Add-on amount:
-if add_onRand <= 10:
-    add_onAmount = 0
-elif add_onRand > 10 and add_onRand <= 25:
-    add_onAmount = 1
-elif add_onRand > 25 and add_onRand <= 75:
-    add_onAmount = 2
-elif add_onRand > 75 and add_onRand <= 89:
-    add_onAmount = 1
-else:
-    add_onAmount = 0
-
+# Two peak days that will have a x2 amount of customers:
 peakDayOne = datetime.datetime(2023,5,5)
-peakDayTWo = datetime.datetime(2023,7,4)
+peakDayTwo = datetime.datetime(2023,7,4)
 
-order_id = 1
 
-oldDate = datetime.datetime(2023,2,22)
+oldDate = datetime.datetime(2023,2,21)
+
 # The dates for the data:
-for i in range(1):
-    currentDate = (oldDate + timedelta(i)).strftime("%Y-%m-%d")
+for i in range(365):
+    currentDate = (oldDate + timedelta(i))
 
+    # Getting a random amount of people:
     peopleRand = random.randint(0,100)
 
     if peopleRand <= 10:
@@ -70,14 +41,19 @@ for i in range(1):
     else:
         peopleAmount = 300
 
+    # If the date is one of the peak days:
+    if (currentDate == peakDayOne) or (currentDate == peakDayTwo):
+        peopleAmount = peopleAmount*2
+
+
     # The amount of orders in a day:
     for j in range(peopleAmount):
 
-        # Random values for menu, drink, add-on amounts:
+        # Random values for menu, drink, add-on amounts and operator:
         menuRand = random.randint(0,100)
         drinkRand = random.randint(0,100)
-        add_onRand = random.randint(0,100)
-        operatorRand = random.randint(0,1)
+        addOnRand = random.randint(0,100)
+        operatorRand = random.randint(1,2)
 
 
         # Finding the amount of menu items:
@@ -92,6 +68,8 @@ for i in range(1):
         else:
             menuAmount = 5
 
+        print(menuAmount)
+
         # Finding the amount of drinks:
         if drinkRand <= 10:
             drinkAmount = 0
@@ -104,116 +82,74 @@ for i in range(1):
         else:
             drinkAmount = 0
 
+        print(drinkAmount)
+
         # Add-on amount:
-        if add_onRand <= 10:
-            add_onAmount = 0
-        elif add_onRand > 10 and add_onRand <= 25:
-            add_onAmount = 1
-        elif add_onRand > 25 and add_onRand <= 75:
-            add_onAmount = 2
-        elif add_onRand > 75 and add_onRand <= 89:
-            add_onAmount = 1
+        if addOnRand <= 10:
+            addOnAmount = 0
+        elif addOnRand > 10 and addOnRand <= 25:
+            addOnAmount = 1
+        elif addOnRand > 25 and addOnRand <= 75:
+            addOnAmount = 2
+        elif addOnRand > 75 and addOnRand <= 89:
+            addOnAmount = 1
         else:
-            add_onAmount = 0
-        #Call function to get price amount
-        #ID is i+j
-        #Operator id is operatorRand
-        #Date of sale is currentDate
+            addOnAmount = 0
+
+        print(addOnAmount)
+
+
+        # Randomizing the time of the order:
+        randHour = random.randint(11,20)
+        randMin = random.randint(0,59)
+
+        randTime = datetime.time(hour=randHour, minute=randMin)
+            
+        timestamp = (datetime.datetime.combine(currentDate, randTime))   
+
+        # String that will execute everything at one time
+        finalExecutable = ""   
+
+
+        # Making the orders with blank total cost:
+        executableOrder = f"INSERT INTO customer_order (date_of_sale, operator_id) VALUES ('{timestamp}', {operatorRand}) returning id;"
+
+        cur.execute (executableOrder)
+
+
+        orderId = cur.fetchone()[0]
+
+        # Making the menu item and order junction table:
+        for k in range(menuAmount):
+
+            randMenuitem = random.randint(1,20)
+
+            executableMenuItem = f"INSERT INTO menu_order (menu_item_id,customer_order_id) VALUES ({randMenuitem}, {orderId});"
+
+            finalExecutable = finalExecutable + "\n" + executableMenuItem
+        
+        # Making the add on and order junction table:
+        for l in range(addOnAmount):
+            randAddOn = random.randint(1,32)
+
+            executableAddOn = f"INSERT INTO add_on_order (ingredient_id,customer_order_id) VALUES ({randAddOn}, {orderId});"
+
+            finalExecutable = finalExecutable + "\n"  + executableAddOn
+        
+        # Making the drink and order junction table:
+        for m in range(drinkAmount):
+            randDrink = random.randint(1,3)
+
+            executableDrink = f"INSERT INTO drink_order (drink_id,customer_order_id) VALUES ({randDrink}, {orderId});"
+
+            finalExecutable = finalExecutable + "\n"  + executableDrink
+        
+        # Executes the final output in one go so there is less back and forth
+        cur.execute(finalExecutable)
+
+
         
 
-        # Creating the junction table for drinkAmount
-        def rand_drink():
-            drink = random.randint(1, 3)  # Generates a random drink ID between 1 and 3 (inclusive)
-            return drink
+    
 
-        def insert_order(order_id, drink):
-            with open('drink_order.csv', 'a', newline='') as csvfile:
-                fieldnames = ['order_id', 'drink_id']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                
-                # Write the order to the CSV file
-                writer.writerow({'order_id': order_id, 'drink_id': drink})
 
-        for k in range(drinkAmount):
-            drink = rand_drink()
-            insert_order(order_id, drink)   
-
-        # Creating the junction table entries for menu items:
-        def rand_menu_item():
-            return random.randint(1, 20)  # Generates a random menu item ID between 1 and 20 (inclusive)
-
-        def insert_menu_item(order_id, menu_item):
-            with open('menu_order.csv', 'a', newline='') as csvfile:
-                fieldnames = ['order_id', 'menu_item_id']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                
-                # Write the menu item to the CSV file
-                writer.writerow({'order_id': order_id, 'menu_item_id': menu_item})
-
-        for l in range(menuAmount):
-            menu_item = rand_menu_item()
-            insert_menu_item(order_id, menu_item)
-
-        # Creating the junction table entries for add_ons:
-        def rand_addon():
-            return random.randint(1, 14)  # Generates a random addon ID between 1 and 22 (inclusive)
-
-        def insert_addon(order_id, addon):
-            with open('add_on_order.csv', 'a', newline='') as csvfile:
-                fieldnames = ['order_id', 'addon_id']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                
-                # Write the addon to the CSV file
-                writer.writerow({'order_id': order_id, 'addon_id': addon})
-
-        for k in range(add_onAmount):
-            addon = rand_addon()
-            insert_addon(order_id, addon)
-        
-        # Adding the cost of all the items together
-        def get_price(id, filename):
-            with open(filename, 'r') as csvfile:
-                reader = csv.DictReader(csvfile)
-                for row in reader:
-                    if int(row['id']) == id:
-                        return float(row['price'])
-            return None  # Return None if item id is not found
-
-        def calculate_order_cost(order_id):
-            total_cost = 0.0
-            
-            # Look into the junction table for menu items
-            with open('menu_order.csv', 'r') as csvfile:
-                reader = csv.DictReader(csvfile)
-                next(reader)
-                for row in reader:
-                    if int(row['order_id']) == order_id:
-                        menu_item_id = int(row['menu_item_id'])
-                        price = get_price(menu_item_id, 'menu.csv')
-                        if price:
-                            total_cost += price
-            
-            # Look into the junction table for drinks
-            with open('drink_order.csv', 'r') as csvfile:
-                reader = csv.DictReader(csvfile)
-                next(reader)
-                for row in reader:
-                    if row['order_id'] == order_id:
-                        drink_id = int(row['drink_id'])
-                        price = get_price(drink_id, 'drink.csv')
-                        if price:
-                            total_cost += price
-            
-            # Look into the junction table for addons
-            with open('add_on_order.csv', 'r') as csvfile:
-                reader = csv.DictReader(csvfile)
-                next(reader)
-                for row in reader:
-                    if int(row['order_id']) == order_id:
-                        addon_id = int(row['addon_id'])
-                        price = get_price(addon_id, 'addon.csv')
-                        if price:
-                            total_cost += price
-            
-            return total_cost
-        order_id = order_id + 1
