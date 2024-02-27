bcd463cf59b0bada898d26cd46add023b5e64e32
Alexander Cox <alexander_cox@tamu.edu>
2024-02-26 18:29:43 -0600

    Changed how its done so it is more intuitive when looking at it

--- Queries/top10Days.sql
+++ Queries/top10Days.sql
@@ -1,7 +1,6 @@
-SELECT CAST(date_of_sale as date), SUM(cost) as total_cost
-from customer_order
-group by CAST(date_of_sale as date)
+SELECT date_trunc('day',date_of_sale) as day_of_sale, SUM(cost) as total_cost, COUNT(*) as number_of_orders
+FROM customer_order
+GROUP BY day_of_sale
 ORDER BY total_cost DESC
 LIMIT 10;
-
 
