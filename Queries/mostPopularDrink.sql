eb1f02599721dbb079d6058823be25a1e318415c
Alexander Cox <alexander_cox@tamu.edu>
2024-02-28 14:24:59 -0600

    Didn't work since name doesn't exist in drinks table. Changed to size

--- Queries/mostPopularDrink.sql
+++ Queries/mostPopularDrink.sql
@@ -1,6 +1,6 @@
 # Getting the most popular menu items in desending order:
-SELECT ij.name AS item_name, COUNT(*) AS total_items
+SELECT ij.size AS item_name, COUNT(*) AS total_items
 FROM drink_order as i
 INNER JOIN drinks AS ij on i.drink_id = ij.id
-GROUP BY ij.name
+GROUP BY ij.size
 ORDER BY total_items DESC;
