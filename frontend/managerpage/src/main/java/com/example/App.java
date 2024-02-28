4798d3976c7083ab445e7aade5b4e1894cb03278
adintyger <adintyger@gmail.com>
2024-02-27 19:35:16 -0600

    basic boilerplate for javafx added

---
+++ frontend/managerpage/src/main/java/com/example/App.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ frontend/managerpage/src/main/java/com/example/PrimaryController.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class PrimaryController {
+
+}
