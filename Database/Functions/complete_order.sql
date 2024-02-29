4c128386d116016983920b682cd143a4a1cef7c7
Alexander Cox <alexander_cox@tamu.edu>
2024-02-28 18:47:01 -0600

    Error fixing

--- Database/Functions/complete_order.sql
+++ Database/Functions/complete_order.sql
@@ -1,15 +1,19 @@
 CREATE FUNCTION complete_order(order_id int, menuItems text[], drinkItems text[], addOns text[]) 
 RETURNS void AS $$
+DECLARE
+	addOn text;
+	menuItem text;
+	drinkItem text;
 BEGIN
     FOREACH menuItem IN ARRAY menuItemsId LOOP
-        PREFORM new_menu_item(menuItem, order_id)
+        PERFORM new_menu_item(menuItem, order_id);
     end loop;
     FOREACH drinkItem IN ARRAY drinkItems LOOP
-        PREFORM new_drink(drinkItem, order_id)
+        PERFORM new_drink(drinkItem, order_id);
     end loop;
     FOREACH addOn IN ARRAY addOns LOOP
-        PREFORM new_add_on(addOn, order_id)
+        PERFORM new_add_on(addOn, order_id);
     end loop;
-    PREFORM dec_stock_order(order_id)
+    PERFORM dec_stock_order(order_id);
 END;
 $$ LANGUAGE plpgsql;
