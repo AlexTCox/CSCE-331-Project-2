507bfe2381ff50bb20b5ac10c047777505fb87df
adintyger <adintyger@gmail.com>
2024-02-28 19:51:45 -0600

    added population for check boxes on init

--- frontend/managerpage/src/main/java/com/example/App.java
+++ frontend/managerpage/src/main/java/com/example/App.java
@@ -20,32 +20,9 @@ import java.io.IOException;
 
         @Override
         public void start(Stage stage) throws IOException {
-            VBox root = new VBox(10); // Container to hold CheckBoxes
-            String user = "csce331_550_01_user";
-            String pswd = "cSCUE8w9";
-            try {
-                Connection connection = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/csce331_550_01_db", user, pswd);
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT name, stock FROM ingredients");
-    
-                while (resultSet.next()) {
-                    String name = resultSet.getString("name");
-                    CheckBox checkBox = new CheckBox(name);
-                    root.getChildren().add(checkBox); // Add CheckBox to VBox
-                }
-                resultSet.close();
-                statement.close();
-                connection.close();
-    
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.err.println(e.getClass().getName() + ": " + e.getMessage());
-                System.exit(0);
-            }
-    
-            Scene scene = new Scene(root, 600, 500, Color.BLUE);
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-    
+             // Container to hold CheckBoxes
+            Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
+            Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
         }
--- frontend/managerpage/src/main/java/com/example/PrimaryController.java
+++ frontend/managerpage/src/main/java/com/example/PrimaryController.java
@@ -1,17 +1,91 @@
 package com.example;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.shape.Circle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-//test
-public class PrimaryController {    
-@FXML
-    public void btnAdd(ActionEvent actionEvent) {
-        // Code to add a new button on the screen
-    }
+import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class PrimaryController implements Initializable{
+
+    @FXML
+    private VBox vboxIngredients;
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet resultSet;
+
+    public void Query(ActionEvent e) throws SQLException {
+        String dbUrl = "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce331_550_01_db";
+        String user = "csce331_550_01_user";
+        String password = "cSCUE8w9";
+
+        String query = "SELECT * FROM menu_Item;";
+        List<Button> buttonlist = new ArrayList<>();
+
+        try {
+            connection = DriverManager.getConnection(dbUrl, user, password);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()) { //iterate over every row returned
+                String restaurant = resultSet.getString("name");
+                buttonlist.add(new Button(restaurant));
+            }
+            vboxIngredients.getChildren().clear();
+            vboxIngredients.getChildren().addAll(buttonlist);
+
+        } catch (SQLException ex) {
+                ex.printStackTrace();
+        } finally {
+            try {
+                resultSet.close();
+                statement.close();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }  
         
-    }
 
+    
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String dbUrl = "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce331_550_01_db";
+        String user = "csce331_550_01_user";
+        String password = "cSCUE8w9";
+
+        String queryIngredients = "SELECT * FROM menu_Item;";
+        List<CheckBox> ingredientsList = new ArrayList<>();
+        List<CheckBox> menuList = new ArrayList<>();
+        List<CheckBox> drinkList = new ArrayList<>();
+
+        try {
+            connection = DriverManager.getConnection(dbUrl, user, password);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(queryIngredients);
+            while (resultSet.next()) { //iterate over every row returned
+                String restaurant = resultSet.getString("name");
+                ingredientsList.add(new CheckBox(restaurant));
+            }
+            vboxIngredients.getChildren().clear();
+            vboxIngredients.getChildren().addAll(ingredientsList);
+
+        } catch (SQLException ex) {
+                ex.printStackTrace();
+        } finally {
+            try {
+                resultSet.close();
+                statement.close();
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    }
