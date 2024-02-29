d481aec5a166afa21d7c15992e0e0a1836ea2ef2
adintyger <adintyger@gmail.com>
2024-02-28 20:05:07 -0600

    added population for all drop down menus

--- frontend/managerpage/src/main/java/com/example/PrimaryController.java
+++ frontend/managerpage/src/main/java/com/example/PrimaryController.java
@@ -1,10 +1,8 @@
 package com.example;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -13,79 +11,72 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class PrimaryController implements Initializable{
+public class PrimaryController implements Initializable {
 
     @FXML
     private VBox vboxIngredients;
+    @FXML
+    private VBox vboxMenuItem;
+    @FXML
+    private VBox vboxDrink;
+
     private static Connection connection;
     private static Statement statement;
     private static ResultSet resultSet;
 
-    public void Query(ActionEvent e) throws SQLException {
-        String dbUrl = "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce331_550_01_db";
-        String user = "csce331_550_01_user";
-        String password = "cSCUE8w9";
-
-        String query = "SELECT * FROM menu_Item;";
-        List<Button> buttonlist = new ArrayList<>();
-
-        try {
-            connection = DriverManager.getConnection(dbUrl, user, password);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(query);
-            while (resultSet.next()) { //iterate over every row returned
-                String restaurant = resultSet.getString("name");
-                buttonlist.add(new Button(restaurant));
-            }
-            vboxIngredients.getChildren().clear();
-            vboxIngredients.getChildren().addAll(buttonlist);
-
-        } catch (SQLException ex) {
-                ex.printStackTrace();
-        } finally {
-            try {
-                resultSet.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }  
-        
-
-    
+    @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         String dbUrl = "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce331_550_01_db";
         String user = "csce331_550_01_user";
         String password = "cSCUE8w9";
 
-        String queryIngredients = "SELECT * FROM menu_Item;";
+        String queryIngredients = "SELECT * FROM ingredients;";
+        String queryMenu = "SELECT * FROM menu_Item;";
+        String queryDrinks = "SELECT * FROM drinks;";
         List<CheckBox> ingredientsList = new ArrayList<>();
         List<CheckBox> menuList = new ArrayList<>();
         List<CheckBox> drinkList = new ArrayList<>();
 
         try {
+            //ingredient population
             connection = DriverManager.getConnection(dbUrl, user, password);
             statement = connection.createStatement();
             resultSet = statement.executeQuery(queryIngredients);
-            while (resultSet.next()) { //iterate over every row returned
-                String restaurant = resultSet.getString("name");
-                ingredientsList.add(new CheckBox(restaurant));
+            while (resultSet.next()) {
+                String ingredient = resultSet.getString("name");
+                ingredientsList.add(new CheckBox(ingredient));
             }
             vboxIngredients.getChildren().clear();
             vboxIngredients.getChildren().addAll(ingredientsList);
 
+            //menu population
+            resultSet = statement.executeQuery(queryMenu);
+            while (resultSet.next()) {
+                String menuItem = resultSet.getString("name");
+                menuList.add(new CheckBox(menuItem));
+            }
+            vboxMenuItem.getChildren().clear();
+            vboxMenuItem.getChildren().addAll(menuList);
+
+            //drink population
+            resultSet = statement.executeQuery(queryDrinks);
+            while (resultSet.next()) {
+                String drink = resultSet.getString("size");
+                drinkList.add(new CheckBox(drink));
+            }
+            vboxDrink.getChildren().clear();
+            vboxDrink.getChildren().addAll(drinkList);
+
         } catch (SQLException ex) {
-                ex.printStackTrace();
+            ex.printStackTrace();
         } finally {
             try {
-                resultSet.close();
-                statement.close();
-                connection.close();
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
         }
     }
-    }
+}
