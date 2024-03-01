dcb26a8948b853f3788cc5386a706397c89871e1
Garrett Moczygemba <gcmoczy@tamu.edu>
2024-02-29 18:17:59 -0600

    test

--- Database/Functions/complete_order.sql
+++ Database/Functions/complete_order.sql
@@ -6,17 +6,6 @@ DECLARE
 	drinkItem text;
 BEGIN
     FOREACH menuItem IN ARRAY menuItemsId LOOP
-<<<<<<< HEAD
-        PERFORM new_menu_item(menuItem, order_id)
-    end loop;
-    FOREACH drinkItem IN ARRAY drinkItems LOOP
-        PERFORM new_drink(drinkItem, order_id)
-    end loop;
-    FOREACH addOn IN ARRAY addOns LOOP
-        PERFORM new_add_on(addOn, order_id)
-    end loop;
-    PERFORM dec_stock_order(order_id)
-=======
         PERFORM new_menu_item(menuItem, order_id);
     end loop;
     FOREACH drinkItem IN ARRAY drinkItems LOOP
@@ -26,6 +15,5 @@ BEGIN
         PERFORM new_add_on(addOn, order_id);
     end loop;
     PERFORM dec_stock_order(order_id);
->>>>>>> 085fffb6e2d76fa5f1ae1463ff66c54711bb9b7a
 END;
 $$ LANGUAGE plpgsql;
