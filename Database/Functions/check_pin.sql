098e8f2facb59ec2ecb3455607f0496cfe6b0086
matthewdmar1 <156950805+matthewdmar1@users.noreply.github.com>
2024-03-03 22:22:46 -0600

    Added comments to the code

--- Database/Functions/check_pin.sql
+++ Database/Functions/check_pin.sql
@@ -1,13 +1,27 @@
+-- Description: This function checks whether a given hashed PIN matches the stored hashed PIN for a specific username in the 'operator' table.
+
+-- Parameters:
+-- @username: The username for which to check the hashed PIN.
+-- @hashed_pin: The hashed PIN to compare against the stored hashed PIN.
+
+-- Returns:
+-- Boolean: True if the hashed PIN matches the stored hashed PIN for the username, False otherwise.
+
 CREATE FUNCTION check_pin(username text, hashed_pin int)
 RETURNS boolean AS $$
 DECLARE
     stored_pin_hash integer;
 BEGIN
-    SELECT pin_hash FROM operator WHERE name = username  INTO stored_pin_hash;
-    if hashed_pin = stored_pin_hash then 
+    -- Select the 'pin_hash' column value from the 'operator' table where the 'name' matches the provided username.
+    -- Assign the result to the 'stored_pin_hash' variable.
+    SELECT pin_hash FROM operator WHERE name = username INTO stored_pin_hash;
+    
+    -- Compare the provided hashed PIN with the stored hashed PIN.
+    -- If they match, return true; otherwise, return false.
+    IF hashed_pin = stored_pin_hash THEN
         RETURN true;
-    else 
+    ELSE
         RETURN false;
-    end if;
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
