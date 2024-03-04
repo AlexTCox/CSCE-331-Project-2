872aee15085b5cd6e5d15057a77a5f6657643786
matthewdmar1 <156950805+matthewdmar1@users.noreply.github.com>
2024-03-03 22:22:03 -0600

    Added comments to the code

--- Database/Functions/check_admin.sql
+++ Database/Functions/check_admin.sql
@@ -1,9 +1,21 @@
+-- Description: This function checks whether a given username belongs to an admin user in the 'operator' table.
+
+-- Parameters:
+-- @username: The username to check for admin privileges.
+
+-- Returns:
+-- Boolean: True if the user is an admin, False otherwise.
+
 CREATE FUNCTION check_admin(username text)
 RETURNS boolean AS $$
 DECLARE
     is_admin boolean;
 BEGIN
-    SELECT admin FROM operator WHERE name = username  INTO is_admin;
+    -- Select the 'admin' column value from the 'operator' table where the 'name' matches the provided username.
+    -- Assign the result to the 'is_admin' variable.
+    SELECT admin FROM operator WHERE name = username INTO is_admin;
+    
+    -- Return the value of 'is_admin', indicating whether the user is an admin or not.
     RETURN is_admin;
 END;
 $$ LANGUAGE plpgsql;
