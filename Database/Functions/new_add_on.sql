0659f700ff878e1f177d65ec938d8d1d6eb31bad
matthewdmar1 <156950805+matthewdmar1@users.noreply.github.com>
2024-03-03 22:30:48 -0600

    Added comments to the code

--- Database/Functions/new_add_on.sql
+++ Database/Functions/new_add_on.sql
@@ -1,10 +1,23 @@
+-- Description: This function adds a new add-on to a specified customer order.
+
+-- Parameters:
+-- @add_on: The name of the add-on to be added.
+-- @order_id: The ID of the customer order to which the add-on will be added.
+
+-- Returns: Void
+
 CREATE FUNCTION new_add_on(add_on text, order_id integer) 
 RETURNS void AS $$
 DECLARE
     add_on_store integer;
 BEGIN
-    SELECT id FROM ingredients WHERE ingredients.name = add_on INTO add_on_store;
-    INSERT INTO  add_on_order(ingredient_id, customer_order_id) VALUES (add_on_store, order_id);
+    -- Retrieve the ID of the add-on from the 'ingredients' table based on its name.
+    SELECT id FROM ingredients WHERE name = add_on INTO add_on_store;
+    
+    -- Insert a new record into the 'add_on_order' table with the retrieved add-on ID and the specified order ID.
+    INSERT INTO add_on_order(ingredient_id, customer_order_id) VALUES (add_on_store, order_id);
+    
+    -- Recalculate the total cost of the order after adding the new add-on.
     PERFORM finding_cost(order_id);
 END;
 $$ LANGUAGE plpgsql;
