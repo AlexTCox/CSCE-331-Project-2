3ef93d4637f049d9be65885f42c399e8dba161e4
matthewdmar1 <156950805+matthewdmar1@users.noreply.github.com>
2024-03-03 22:41:09 -0600

    Added comments to the code

--- Database/Functions/new_order.sql
+++ Database/Functions/new_order.sql
@@ -1,9 +1,20 @@
+-- Description: This function creates a new customer order with the specified operator ID and returns the ID of the newly created order.
+
+-- Parameters:
+-- @operator_id: The ID of the operator associated with the new order.
+
+-- Returns: The ID of the newly created order.
+
 CREATE FUNCTION new_order(operator_id integer) 
 RETURNS integer AS $order_id$
-declare
-	order_id integer;
+DECLARE
+    order_id integer;
 BEGIN
-    INSERT INTO customer_order (date_of_sale, operator_id) VALUES (NOW()::TIMESTAMP, operator_id) returning id INTO order_id;
-	RETURN order_id;
+    -- Insert a new record into the 'customer_order' table with the current timestamp and the provided operator ID.
+    -- Return the ID of the newly created order into the 'order_id' variable.
+    INSERT INTO customer_order (date_of_sale, operator_id) VALUES (NOW()::TIMESTAMP, operator_id) RETURNING id INTO order_id;
+    
+    -- Return the ID of the newly created order.
+    RETURN order_id;
 END;
 $order_id$ LANGUAGE plpgsql;
