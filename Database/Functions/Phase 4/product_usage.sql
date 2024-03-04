ecefbcbfb08d3bfc7cac13ea43b295fb5be8b8e1
matthewdmar1 <156950805+matthewdmar1@users.noreply.github.com>
2024-03-03 22:50:17 -0600

    Added comments to the code

--- Database/Functions/Phase 4/product_usage.sql
+++ Database/Functions/Phase 4/product_usage.sql
@@ -1,17 +1,25 @@
+-- Description: This function generates a report of product usage within a specified time period.
+
+-- Parameters:
+-- @start_date: The start date of the time period for which to generate the report.
+-- @end_date: The end date of the time period for which to generate the report.
+
+-- Returns: Table containing the date and the count of product usage for each day within the specified time period.
+
 CREATE FUNCTION product_usage(start_date timestamp, end_date timestamp)
 RETURNS TABLE (
     date timestamp,
-    count bigint)
-AS $$
+    count bigint
+) AS $$
 BEGIN
-    RETURN QUERY SELECT date_trunc('day',date_of_sale) as day_of_sale, COUNT(*) as total_sales
-    FROM menu_item_ingredient
-    INNER JOIN menu_item ON menu_item.id = menu_item_ingredient.menu_id
-    INNER JOIN menu_order ON menu_order.menu_item_id = menu_item.id
-    INNER JOIN customer_order ON customer_order.id = menu_order.customer_order_id
-    WHERE date_of_sale BETWEEN start_date AND end_date
-    GROUP BY day_of_sale
-    ORDER BY day_of_sale DESC;
-    
+    -- Query to retrieve the count of product usage for each day within the specified time period.
+    RETURN QUERY SELECT date_trunc('day', date_of_sale) AS date, COUNT(*) AS count
+                 FROM menu_item_ingredient
+                 INNER JOIN menu_item ON menu_item.id = menu_item_ingredient.menu_id
+                 INNER JOIN menu_order ON menu_order.menu_item_id = menu_item.id
+                 INNER JOIN customer_order ON customer_order.id = menu_order.customer_order_id
+                 WHERE date_of_sale BETWEEN start_date AND end_date
+                 GROUP BY date
+                 ORDER BY date DESC;
 END;
 $$ LANGUAGE plpgsql;
