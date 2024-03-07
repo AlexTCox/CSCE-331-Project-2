7536824b768f7927bb5d77ca9c8fb02006b64200
adintyger <adintyger@gmail.com>
2024-03-06 19:16:34 -0600

    removed garbage files

--- frontend/LoginPage/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.loginpage {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.loginpage to javafx.fxml;
-    exports com.example.loginpage;
-}
--- frontend/managerpage/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example to javafx.fxml;
-    exports com.example;
-}
